9eda6114f8f9d9676ebdafe9e7dca17466452421
carlos.moreno <carlos.moreno@swiftmedical.com>
2024-09-19 00:56:42 -0500

    - upload an image by language separates per tabs - Set background color for all cards - allow change the description and title of the card - allow link edition using a popup - modify the remplate html - allow create maximun 10 cards

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,24 @@
     let scale = 1;
 	var img = document.getElementById('image');
 	var imgContainer = document.getElementById('image-container');
+	var imgSpanish = document.getElementById('imageSpanish');
+	var imgContainerSpanish = document.getElementById('image-container-Spanish');
 	var scaleRange = document.getElementById('scaleRange');
+	var scaleRangeSpanish = document.getElementById('scaleRangeSpanish');
     var eyebutton = document.getElementById('eyebutton');
     var showcard = document.getElementById('showcard');
     let offsetX = 0;
     let offsetY = 0;
     let isDragging = false;
     let dragOffsetX = 0, dragOffsetY = 0;
+    let previustabFocusedId = "tab1";
+    const MAX_NUMBER_TABS = 10;
 
 	// Load the selected image into the container
-    function loadImage(event) {
-      const reader = new FileReader();	  
+    function loadImage(event, imgId, scaleId) {
+      const reader = new FileReader();	 
+      let img  = document.getElementById(imgId);
+      let scaleRange  = document.getElementById(scaleId);
       reader.onload = function() {
         img.src = reader.result;
         img.style.width = '100%';  // reset to original scale
@@ -24,37 +31,57 @@
       reader.readAsDataURL(event.target.files[0]);
     }
 
-    // Dragging functionality for image
-    img.addEventListener('mousedown', (event) => {
-      isDragging = true;
-      const rect = img.getBoundingClientRect();
-      dragOffsetX = event.clientX - rect.left;
-      dragOffsetY = event.clientY - rect.top;
-      event.preventDefault();
-    });
-
-    document.addEventListener('mousemove', (event) => {
-      if (isDragging) {
-        const containerRect = imgContainer.getBoundingClientRect();
-        const newLeft = event.clientX - dragOffsetX - containerRect.left;
-        const newTop = event.clientY - dragOffsetY - containerRect.top;
-
-        img.style.left = newLeft + 'px';
-        img.style.top = newTop + 'px';
-      }
-    });
-
-    document.addEventListener('mouseup', () => {
-      isDragging = false;
-    });
+    function addEventsImages(img, imgContainer){
+        img.addEventListener('mousedown', (event) => {
+            isDragging = true;
+            const rect = img.getBoundingClientRect();
+            dragOffsetX = event.clientX - rect.left;
+            dragOffsetY = event.clientY - rect.top;
+            event.preventDefault();
+        });
+    
+        img.addEventListener('mousemove', (event) => {
+        if (isDragging) {
+            let img = document.getElementById(event.target.id);
+            if(img != null){;
+                const containerRect = imgContainer.getBoundingClientRect();
+                const newLeft = event.clientX - dragOffsetX - containerRect.left;
+                const newTop = event.clientY - dragOffsetY - containerRect.top;
+                img.style.left = newLeft + 'px';
+                img.style.top = newTop + 'px';
+            }
+        }
+        });
+    
+        imgContainer.addEventListener('mouseup', () => {
+            isDragging = false;
+        });
+    }
+    addEventsImages(img, imgContainer);
+    addEventsImages(imgSpanish, imgContainerSpanish);    
 
     // Scaling functionality with range input
     document.getElementById('scaleRange').addEventListener('input', (event) => {
       const scale = event.target.value / 100;
+      let img = document.getElementById(event.target.getAttribute("data-target-id"));
       img.style.width = (scale * 100) + '%';
       img.style.height = (scale * 100) + '%';
     });
-	
+
+    document.getElementById('scaleRangeSpanish').addEventListener('input', (event) => {
+      const scale = event.target.value / 100;
+      let img = document.getElementById(event.target.getAttribute("data-target-id"));
+      img.style.width = (scale * 100) + '%';
+      img.style.height = (scale * 100) + '%';
+    });
+	function validateAddTab(){     
+        if(tabCount >= MAX_NUMBER_TABS) {
+            document.getElementById("add-tab").style.display = "none"
+            return;
+        }else{   
+            document.getElementById("add-tab").style.display = "block"
+        }
+    }
     function addNewTab() {
         let tabCounter = (tabCount++);
         const tabId = 'tab' + tabCount;
@@ -65,7 +92,7 @@
         newTab.classList.add('nav-item');
         newTab.innerHTML = `
             <button class="nav-link" id="${tabId}" data-bs-toggle="tab" onclick="focusedTab('-${tabCounter}')" data-bs-target="#${tabPaneId}" type="button" role="tab" aria-controls="${tabPaneId}" aria-selected="false">
-                <span class="editable-title" ondblclick="makeTitleEditable(this)" onblur="updateTabTitle(this, '${tabId}')" onkeydown="checkEnter(event, this)">Tab ${tabCount}</span>
+                <span class="editable-title" ondblclick="makeTitleEditable(this)" onblur="updateTabTitle(this, '${tabId}')" onkeydown="checkEnter(event, this)">Card ${tabCount}</span>
                 <span class="close-tab" onclick="removeTab(event, '${tabId}')">&times;</span>
             </button>
         `;
@@ -81,7 +108,8 @@
         newTabPane.innerHTML = GetTemplate(tabCounter);
         document.getElementById('tabContent').appendChild(newTabPane);	
 		focusedTab('-' + tabCounter);
-        document.getElementById(tabId).click();
+        document.getElementById(tabId).click();        
+        validateAddTab();
     }
 
     function removeTab(event, tabId) {
@@ -100,44 +128,39 @@
             firstTab.classList.add('active');
             firstTabPane.classList.add('show', 'active');
         }
+        tabCount = (tabCount-1);
+        validateAddTab();
     }
 
     function focusedTab(tabcount) {	
 		img = document.getElementById('image' + tabcount);
+		imageSpanish = document.getElementById('imageSpanish' + tabcount);
 		imgContainer = document.getElementById('image-container' + tabcount);
+		imgContainerSpanish = document.getElementById('image-container-Spanish' + tabcount);
         scaleRange = document.getElementById('scaleRange' + tabcount);
+        scaleRangeSpanish = document.getElementById('scaleRangeSpanish' + tabcount);
         eyebutton = document.getElementById('eyebutton' + tabcount);
         showcard = document.getElementById('showcard' + tabcount);
-		
-		img.addEventListener('mousedown', (event) => {
-		  isDragging = true;
-		  const rect = img.getBoundingClientRect();
-		  dragOffsetX = event.clientX - rect.left;
-		  dragOffsetY = event.clientY - rect.top;
-		  event.preventDefault();
-		});
-
-		document.addEventListener('mousemove', (event) => {
-		  if (isDragging) {
-			const containerRect = imgContainer.getBoundingClientRect();
-			const newLeft = event.clientX - dragOffsetX - containerRect.left;
-			const newTop = event.clientY - dragOffsetY - containerRect.top;
-
-			img.style.left = newLeft + 'px';
-			img.style.top = newTop + 'px';
-		  }
-		});
-
-		document.addEventListener('mouseup', () => {
-		  isDragging = false;
-		});
+        
+        addEventsImages(img, imgContainer);
+        addEventsImages(imageSpanish, imgContainerSpanish);
 
 		// Scaling functionality with range input
 		document.getElementById('scaleRange' + tabcount).addEventListener('input', (event) => {
 		  const scale = event.target.value / 100;
+          let img = document.getElementById(event.target.getAttribute("data-target-id"));
 		  img.style.width = (scale * 100) + '%';
 		  img.style.height = (scale * 100) + '%';
 		});
+
+		// Scaling functionality with range input
+		document.getElementById('scaleRangeSpanish' + tabcount).addEventListener('input', (event) => {
+		  const scale = event.target.value / 100;          
+          let img = document.getElementById(event.target.getAttribute("data-target-id"));
+		  img.style.width = (scale * 100) + '%';
+		  img.style.height = (scale * 100) + '%';
+		});
+        previustabFocusedId = tabcount;
     }	
 
     // Make the tab title editable on double click
@@ -172,6 +195,38 @@
             eyebutton.classList.remove("bi-eye-slash");
             eyebutton.classList.add("bi-eye")
         }
+    }
+
+    function changeBackgroundColor(element){
+        var elementCard = document.querySelectorAll('div.card');
+        let backColor = element.getAttribute("backcolor");
+        elementCard.forEach((element) => { 
+            element.style.backgroundColor = backColor;
+        });
+    }
+
+    function changeTextContent(element){
+
+        let datatargetids = element.getAttribute("data-target-id").split(',');
+
+        datatargetids.forEach((el) => { 
+            let cardtitle = document.getElementById(el);
+            cardtitle.innerText = element.value;
+        });
+    }
+
+    function changeLinkUrl(element, tabcount){        
+        document.getElementById('cardlink' + tabcount).setAttribute("data-target-id", element.id);
+        document.getElementById('linkName' + tabcount).value = element.innerText;
+        document.getElementById('linkUrl' + tabcount).value = element.getAttribute("href");
+    }
+
+    function submitLink(tabcount) {
+        let readmorelinkId = document.getElementById('cardlink' + tabcount).getAttribute("data-target-id");
+        let readmorelink = document.getElementById(readmorelinkId);
+        readmorelink.innerText = document.getElementById('linkName' + tabcount).value;
+        readmorelink.setAttribute("href", document.getElementById('linkUrl' + tabcount).value); 
+        document.getElementById('hiddenButtonModal' + tabcount).click();
     }
 
     
--- src/js/templates/templates.js
+++ src/js/templates/templates.js
@@ -2,57 +2,140 @@ const teplateContentTab = `
                 <div class="row mt-4">                
                 <input type="hidden" id="showcard{tabCounter}" value="true"/>
     <div class="form-group row">
-        <label for="scaleRange{tabCounter}" class="col-sm-1 col-form-label">Size</label>
-        <div class="col-sm-2">
-            <input type="range" class="form-control-range mt-3" id="scaleRange{tabCounter}" min="10" max="200" value="100">
+        <div class="col-sm-1">
+        </div>
+        <div class="col-sm-2">						
         </div>
         <div class="col-sm-4">						
         </div>
         <div class="col-sm-4 pipe">
             <a href="#" id="eyebutton{tabCounter}" class="bi bi-eye icontool" onclick="ShowHideCard()"></a>
-			<a href="#" class="bi bi-floppy icontool"></a>
+            <a href="#" class="bi bi-floppy icontool"></a>
         </div>
     </div>
-    <input type="file" id="file-input{tabCounter}" accept="image/*" onchange="loadImage(event)" class="file-input">
-    <div class="col-md-4">                    
-        <div class="card" style="width: 300px;">
-            <div class="image-wrapper">						  
-                <div id="image-container{tabCounter}" class="image-container card-img-top" ondblclick="document.getElementById('file-input{tabCounter}').click();">
-                <img id="image{tabCounter}" class="image" draggable="false">
-                </div>
-            </div>						
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
+    <input type="file" id="file-input{tabCounter}" accept="image/*" onchange="loadImage(event, 'image{tabCounter}', 'scaleRange{tabCounter}')" class="file-input">
+    <input type="file" id="file-input-Spanish{tabCounter}" accept="image/*" onchange="loadImage(event, 'imageSpanish{tabCounter}', 'scaleRangeSpanish{tabCounter}')" class="file-input">
+    <input type="file" id="file-input-French{tabCounter}" accept="image/*" onchange="loadImage(event, '', '')" class="file-input">
+    <input type="file" id="file-input-Portugues{tabCounter}" accept="image/*" onchange="loadImage(event, '', '')" class="file-input">
+    <div class="col-md-4">    
+                    <ul class="nav nav-tabs" id="tabListLanguagesImages" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="tabEnglish{tabCounter}" data-bs-toggle="tab" data-bs-target="#tabPaneEnglish{tabCounter}" type="button"
+                                    role="tab" aria-controls="tabPaneEnglish{tabCounter}" aria-selected="true">                
+                                <span class="editable-title" ondblclick="makeTitleEditable(this)" onblur="updateTabTitle(this, 'tabEnglish{tabCounter}')" onkeydown="checkEnter(event, this)">English</span>
+                            </button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="tabSpanish{tabCounter}" data-bs-toggle="tab" data-bs-target="#tabPaneSpanish{tabCounter}" type="button"
+                                    role="tab" aria-controls="tabPaneSpanish{tabCounter}" aria-selected="true">                
+                                <span class="editable-title" ondblclick="makeTitleEditable(this)" onblur="updateTabTitle(this, 'tabEnglish{tabCounter}')" onkeydown="checkEnter(event, this)">Spanish</span>
+                            </button>
+                        </li>  
+                    </ul> 
+                    <div class="tab-content" id="tabContentLanguagesImagesEnglish">
+                        <!-- Tab 1 Content -->
+                        <div class="tab-pane fade show active" id="tabPaneEnglish{tabCounter}" role="tabpanel" aria-labelledby="tabEnglish{tabCounter}">
+                            <div class="form-group row">
+                                <label for="scaleRange{tabCounter}" class="col-sm-2 col-form-label">Size</label>
+                                <div class="col-sm-2">
+                                    <input type="range" class="form-control-range mt-3" id="scaleRange{tabCounter}" data-target-id="image{tabCounter}" min="10" max="200" value="100">
+                                </div>
+                            </div>
+                            <div class="card" style="width: 300px;">
+                                <div class="image-wrapper">						  
+                                  <div id="image-container{tabCounter}" class="image-container card-img-top" ondblclick="document.getElementById('file-input{tabCounter}').click();">
+                                    <img id="image{tabCounter}" class="image" draggable="false">
+                                  </div>
+                                </div>						
+                                <div class="card-body">
+                                    <h5 class="card-title" id="cardtitle{tabCounter}">Card title</h5>
+                                    <p class="card-text" id="cardText{tabCounter}">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="readmorelink" id="readmorelink{tabCounter}" data-toggle="modal" data-target="#linkModal{tabCounter}" onclick="changeLinkUrl(this, {tabCounter})">read more</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="tabPaneSpanish{tabCounter}" role="tabpanel" aria-labelledby="tabSpanish{tabCounter}">
+                            <div class="form-group row">
+                                <label for="scaleRangeSpanish{tabCounter}" class="col-sm-2 col-form-label">Size</label>
+                                <div class="col-sm-2">
+                                    <input type="range" class="form-control-range mt-3" id="scaleRangeSpanish{tabCounter}" data-target-id="imageSpanish{tabCounter}" min="10" max="200" value="100">
+                                </div>
+                            </div>
+                            <div class="card" style="width: 300px;">
+                                <div class="image-wrapper">						  
+                                  <div id="image-container-Spanish{tabCounter}" class="image-container card-img-top" ondblclick="document.getElementById('file-input-Spanish{tabCounter}').click();">
+                                    <img id="imageSpanish{tabCounter}" class="image" draggable="false">
+                                  </div>
+                                </div>						
+                                <div class="card-body">
+                                    <h5 class="card-title" id="cardtitleSpanish{tabCounter}">Card title</h5>
+                                    <p class="card-text" id="cardTextSpanish{tabCounter}">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="readmorelink" id="readmorelinkSpanish{tabCounter}" data-toggle="modal" data-target="#linkModal{tabCounter}" onclick="changeLinkUrl(this, {tabCounter})">read more</a>
+                                </div>
+                            </div>
+                        </div>   
+                        <div class="modal fade" id="linkModal{tabCounter}" tabindex="-1" role="dialog" aria-labelledby="linkModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="linkModalLabel{tabCounter}">Add a Link</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close" id="hiddenButtonModal{tabCounter}">
+                                            <span aria-hidden="true">×</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form id="linkForm{tabCounter}">
+                                            <input type="hidden" id="cardlink{tabCounter}" data-target-id="">
+                                            <div class="form-group">
+                                                <label for="linkName">Link Name</label>
+                                                <input type="text" class="form-control" id="linkName{tabCounter}" data-target-id="" required>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="linkUrl">Link URL</label>
+                                                <input type="url" class="form-control" id="linkUrl{tabCounter}" data-target-id="" required>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary" onclick="submitLink({tabCounter})">Save</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>  
     </div>
     <div class="col-md-6">
         <form>
             <div class="form-group">
             <label for="title{tabCounter}">Title</label>
-            <input type="text" class="form-control" id="title{tabCounter}" placeholder="Enter title">
+            <input type="text" class="form-control" id="title{tabCounter}" data-target-id="cardtitle{tabCounter},cardtitleSpanish{tabCounter}" onchange="changeTextContent(this)" placeholder="Enter title">
             </div>
             <div class="form-group">
             <label for="description{tabCounter}">Description</label>
-            <textarea class="form-control" id="description{tabCounter}" placeholder="Enter description" rows="3"></textarea>
+            <textarea class="form-control" id="description{tabCounter}" data-target-id="cardText{tabCounter},cardTextSpanish{tabCounter}" placeholder="Enter description" rows="3" onchange="changeTextContent(this)"></textarea>
             </div>
             <div class="form-group">
             <label for="legend{tabCounter}">Legend</label>
             <input type="text" class="form-control" id="legend{tabCounter}" placeholder="Enter legend">
             </div>
             <div class="form-group">
-            <input type="color" class="color-picker form-control" id="backgroundColor{tabCounter}">
-            <label for="backgroundColor{tabCounter}">Select Background Color</label>
+                <label for="backgroundColor">Background Color</label>
+                <label class="color-picker form-control backgroundColor1" backcolor="#ffffff" id="backgroundColor1{tabCounter}" onclick="changeBackgroundColor(this)"></label>
+                <label class="color-picker form-control backgroundColor2" backcolor="#f0f0f0" id="backgroundColor2{tabCounter}" onclick="changeBackgroundColor(this)"></label>
+                <label class="color-picker form-control backgroundColor3" backcolor="#6287BA" id="backgroundColor3{tabCounter}" onclick="changeBackgroundColor(this)"></label>
+                <label class="color-picker form-control backgroundColor4" backcolor="#707B89" id="backgroundColor4{tabCounter}" onclick="changeBackgroundColor(this)"></label>
+                <label class="color-picker form-control backgroundColor5" backcolor="#d9d5d1" id="backgroundColor5{tabCounter}" onclick="changeBackgroundColor(this)"></label>
+                <label class="color-picker form-control backgroundColor6" backcolor="#ebe8e5" id="backgroundColor6{tabCounter}" onclick="changeBackgroundColor(this)"></label>
             </div>
-            <div class="form-group">
+            <!-- <div class="form-group">
             <label for="linkUrl{tabCounter}">Link URL</label>
             <input type="url" class="form-control" id="linkUrl{tabCounter}" placeholder="Enter URL">
             </div>
             <div class="form-group">
             <label for="linkName{tabCounter}">Link Name</label>
-            <input type="text" class="form-control" id="linkName{tabCounter}" placeholder="Enter link name">
-            </div>
+            <input type="text" class="form-control" data-target-id="readmorelink{tabCounter},readmorelinkSpanish{tabCounter}" id="linkName{tabCounter}" placeholder="Enter link name" onchange="changeTextContent(this)">
+            </div>-->
         </form>
     </div>
 </div>
